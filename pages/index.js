9d1040102291af0de56d5676da9d7cc71e46d603
João Rodrigues <joao.rodrigues0033@gmail.com>
2024-02-28 22:22:15 -0300

    adiciona arquivo

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,11 @@ import Header from '../components/Header/Header';
 
 function Home() {
 
-    return (
-        <>
-            <Header />
-        </>
-    );
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default Home;
